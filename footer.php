fc1effc7d49ee384d4da5cde47448c6373857db7
Francisco Morán <fjmoran@gmail.com>
2013-07-17 12:26:48 -0400

    Cambia footer a Zhi

--- footer.php
+++ footer.php
@@ -4,6 +4,6 @@
  </div><!-- .container -->
 <div id="footer"> 
 <div class="container">
-    <p class="muted credit text-center"> Desarrollado por: Boutique Developer &copy;, <a href="http://twitter.com/boutiquedev" target="_blank">@boutiquedev</a> </p>
+    <p class="muted credit text-center"> Desarrollado por: Zhi &copy;, <a href="http://twitter.com/boutiquedev" target="_blank">@zhi_chile</a> </p>
 </div>
 </div><!-- .footer --> 
