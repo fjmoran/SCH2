1074935073479baab69a0ecd24e7dc6fc514d4cf
Francisco Morán <fjmoran@gmail.com>
2013-08-07 17:50:45 -0400

    cambios menores

--- footer.php
+++ footer.php
@@ -4,6 +4,6 @@
  </div><!-- .container -->
 <div id="footer"> 
 <div class="container">
-    <p class="muted credit text-center"> Desarrollado por: 只 Zhi &copy;, <a href="http://twitter.com/boutiquedev" target="_blank">@zhi_chile</a> </p>
+    <p class="muted credit text-center"> Desarrollado por: 只 Zhi &copy;, <a href="http://twitter.com/zhi_chile" target="_blank">@zhi_chile</a> </p>
 </div>
 </div><!-- .footer --> 
--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <div class="navbar">
   <div class="navbar-inner">
-    <a class="brand pull-left" href="#">Sistema de Control de Horas</a>
+    <a class="brand pull-left" href="index.php">Sistema de Control de Horas</a>
     <!--
     <h5 class="text-info pull-right"><i class="icon-user" style="vertical-align:bottom;"></i> <span class="label label-inverse">Francisco Morán</span></h5>
 	-->
