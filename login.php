5df15584b4b9dec1813f7a7e5568a2c77d92f776
Francisco Morán <fjmoran@gmail.com>
2013-08-21 12:45:12 -0400

    pasando login a BS3

--- login.php
+++ login.php
@@ -9,18 +9,17 @@
     <meta name="author" content="">
 
     <!-- Le styles -->
-    <link href="recursos/bootstrap/assets/css/bootstrap.css" rel="stylesheet">
+    <link href="recursos/bootstrap3/css/bootstrap.min.css" rel="stylesheet" media="screen">
 
     <link href='http://fonts.googleapis.com/css?family=Ubuntu+Condensed' rel='stylesheet' type='text/css'>
 
     <link href="img/favicon.ico" rel="SHORTCUT ICON">
 
-    <style type="text/css">
+     <style type="text/css">
 
       h2 { font-family: 'Ubuntu Condensed', sans-serif !important; font-weight: 400; }
     
       body {
-        height: 100%;
         background-color: #f5f5f5;
       }
 
@@ -49,27 +48,14 @@
         padding: 7px 9px;
       }
 
-    </style>
-    <link href="../assets/css/bootstrap-responsive.css" rel="stylesheet">
-
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-      <script src="../assets/js/html5shiv.js"></script>
-    <![endif]-->
-
-    <!-- Fav and touch icons -->
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
-      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
-                    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
-                                   <link rel="shortcut icon" href="../assets/ico/favicon.png">
+    </style> 
   </head>
 
   <body>
 
-    <div class="navbar">
-      <div class="navbar-inner">
-      <a class="brand pull-left" href=#>Sistema de Control de Horas</a>
+    <div class="navbar navbar-default" role="navigation">
+      <div class="navbar-header">
+      <a class="navbar-brand pull-left" href="#">Sistema de Control de Horas</a>
       </div>
     </div><!--.navbar-->
 
@@ -77,37 +63,43 @@
 
       <form class="form-signin" name="login" action="index.php" method="post">
         <h2 class="form-signin-heading">Acceso</h2>
-        <input type="text" class="input-block-level" placeholder="Usuario">
-        <input type="password" class="input-block-level" placeholder="Contraseña">
+        <input type="text" class="form-control" placeholder="Usuario">
+        <input type="password" class="form-control" placeholder="Contraseña">
         <label class="checkbox">
           <input type="checkbox" value="remember-me"> Recordarme
         </label>
-        <button class="btn btn-primary" type="submit">Ingresar</button><br><br>
+        <button class="btn btn-primary btn-block" type="submit">Ingresar</button><br>
         <a href="#recuperar" data-toggle="modal">Recuperar contraseña</a>
       </form>
 
     </div> <!-- /container -->
 
       <!-- Modal -->
-      <div id="recuperar" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="recuperarLabel" aria-hidden="true">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-          <h4 id="myModalLabel">¿Olvidaste tu contraseña?</h4>
-        </div>
-        <div class="modal-body">
-          <p>Ingresa tu correo electrónico para recuperarla</p>
-          <input type="email" placeholder="correo@dominio.com">
-        </div>
-        <div class="modal-footer">
-          <button class="btn" data-dismiss="modal" aria-hidden="true">Cerrar</button>
-          <button class="btn btn-primary">Enviar</button>
-        </div>
-      </div>
+      <div id="recuperar" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="recuperarLabel" aria-hidden="true">
+        
+        <div class="modal-dialog">
+          <div class="modal-content">
+
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+              <h4 class="modal-title" id="myModalLabel">¿Olvidaste tu contraseña?</h4>
+            </div>
+            <div class="modal-body">
+              <p>Ingresa tu correo electrónico para recuperarla</p>
+              <input type="email" class="form-control" placeholder="correo@dominio.com"><br>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+              <button type="button" class="btn btn-primary">Enviar</button>
+            </div>
+          </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+      </div><!-- /.modal -->
 
     <!-- javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="recursos/jquery/jquery-1.10.1.min.js"></script>    
-    <script src="recursos/bootstrap/assets/js/bootstrap.min.js"></script>
+    <script src="recursos/bootstrap3/js/bootstrap.min.js"></script>
   </body>
 </html>
