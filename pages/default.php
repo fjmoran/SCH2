6f6497a8c32d432148c6e07561d8e3f8fffe5c57
fjmoran <fjmoran@gmail.com>
2013-09-02 23:20:38 -0400

    carrusel

--- pages/default.php
+++ pages/default.php
@@ -1,5 +1,43 @@
  <div class="col-md-11">
     <h2>Bienvenido!</h2>
     <h5>El sistema de control de horas le proporciona una manera facil y amigable de administrar los recurso de su empresa!</h5>
-    <img src="img/meeting.jpg" class="img-thumbnail img-responsive"> 
+    <div class=col-md-9>
+    <div id="carousel-example-generic" class="carousel slide">
+	  <!-- Indicators -->
+	  <ol class="carousel-indicators">
+	    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+	    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+	    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+	    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+	  </ol>
+
+	  <!-- Wrapper for slides -->
+	  <div class="carousel-inner">
+	    <div class="item active">
+	      <img src="img/slide1.jpg" alt="">
+	    </div>
+	    <div class="item">
+	      <img src="img/slide2.jpg" alt="">
+	    </div>
+	    <div class="item">
+	      <img src="img/slide3.jpg" alt="">
+	    </div>
+	    <div class="item">
+	      <img src="img/slide4.jpg" alt="">
+	    </div>	    	    	    
+	   
+	  </div>
+
+	  <!-- Controls -->
+	  <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+	    <span class="icon-prev"></span>
+	  </a>
+	  <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+	    <span class="icon-next"></span>
+	  </a>
+	</div>
+		</div>
+
+    <!-- <img src="img/meeting.jpg" class="img-thumbnail img-responsive">  -->
+
 </div><!-- .span11 -->
