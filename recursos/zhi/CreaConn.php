6a7c0e81393bb409230cce8649823a601fcf4400
Francisco Morán <fjmoran@gmail.com>
2013-09-06 14:53:59 -0400

    esquema correcto para la bd

--- recursos/zhi/CreaConn.php
+++ recursos/zhi/CreaConn.php
@@ -7,5 +7,5 @@ $host = "localhost";
 import_request_variables("Pg");
 $connection = mysql_connect($host,$user,$passwd)
 or die ("No me pude conectar con MYSQL --> " . mysql_error($conn));
-mysql_select_db("ccdrm",$connection);
+mysql_select_db("SCH2",$connection);
 ?>
