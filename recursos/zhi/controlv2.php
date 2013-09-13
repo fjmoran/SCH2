0401cd9557c896049af13968c7dcb2274a4ed36e
fjmoran <fjmoran@gmail.com>
2013-09-12 21:21:39 -0300

    Controlv2.php
    se agrega en query elemento de usuario activo

--- recursos/zhi/controlv2.php
+++ recursos/zhi/controlv2.php
@@ -3,15 +3,14 @@
 require "CreaConnv2.php";
 
 //Definir consulta
-
-$SSQL="SELECT idUsuario, userUsuario,nombreUsuario,Perfil_idPerfil FROM SCH2.Usuario WHERE Usuario.userUsuario='".$_POST['user']."' and Usuario.claveUsuario='".$_POST['password']."'";
+$SSQL="SELECT idUsuario, userUsuario,nombreUsuario,Perfil_idPerfil FROM SCH2.Usuario WHERE Usuario.userUsuario='".$_POST['user']."' and Usuario.claveUsuario='".$_POST['password']."' and Usuario.activoUsuario=1";
 //Realizar consulta echo "Antes de consulta ".$SSQL."</br>";
 
 if ($rs=$mysqli->query($SSQL)){
 	//Buscar coincidencias
 	if ($rs->num_rows > 0){
 		$row=$rs->fetch_array(MYSQLI_ASSOC);
-		// echo $row[idUsuario].",". $row[userUsuario].",".$row[nombreUsuario].",".$row[Perfil_idPerfil];
+	//	echo $row[idUsuario].",". $row[userUsuario].",".$row[nombreUsuario].",".$row[Perfil_idPerfil];	
 		session_start();
 		$_SESSION[auth]=1;
 		$_SESSION[userUsuario]=$row[userUsuario];
