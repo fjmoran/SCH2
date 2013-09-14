d96cc1488a267377f387c0efc7fec018be085e8d
fjmoran <fjmoran@gmail.com>
2013-09-14 12:56:17 -0300

    form del rol

--- pages_admin/roles_form_base.php
+++ pages_admin/roles_form_base.php
@@ -1,3 +1,38 @@
 	<form role="form">
-		
-	</form>
+    <div class="row"> <!-- fila para sub titulo opcional -->
+      <div class="col-md-12">
+        <h4></h4>
+      </div>
+    </div> 
+    <div class="row"> <!-- fila con 2 columnas -->
+      <div class="col-md-6"> <!-- columna izquierda -->
+        <div class="form-group"> 
+          <label for="nombre_rol">Nombre:</label>
+          <input id="nombre_rol" class="form-control" type="text" placeholder="Nombre del rol">           
+        </div> 
+        <div class="form-group">
+          <label for="estado_rol">Estado:</label>
+            <select id="estado_rol" class="form-control">
+              <option>Activo</option>                          
+              <option>Inactivo</option>                                                                         
+            </select>          
+        </div>                                                             
+      </div>
+      <div class="col-md-6"> <!-- columna derecha -->
+        <div class="form-group">
+          <label for="desc_rol">Descripción:</label>
+          <input id="desc_rol" class="form-control" type="text" placeholder="Descripción del rol"> 
+        </div>                                             
+      </div>  
+    </div>      
+      
+    <div class="row pull-right"> <!-- fila para botones -->
+      <div class="col-md-12">
+        <p>
+          <button class="btn btn-default">Cancelar</button>
+          <button class="btn btn-primary">Guardar</button>
+        </p>
+      </div>
+    </div> 
+
+	</form>	
