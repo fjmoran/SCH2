718716a71a4c21678c30e51906e19a2e57cd611d
Francisco Morán <fjmoran@gmail.com>
2013-09-16 12:39:07 -0300

    sidebar admin

--- sidebar_admin.php
+++ sidebar_admin.php
@@ -14,7 +14,10 @@
         <li onclick="$('#cuerpo').load('pages_admin/default.php');"><a href="#mat_tar"><span class="glyphicon glyphicon-chevron-right"></span> Tarifas por materia</a></li>   
         <li onclick="$('#cuerpo').load('pages_admin/default.php');"><a href="#fact_estdo"><span class="glyphicon glyphicon-chevron-right"></span> Estados de facturas</a></li>
         <li onclick="$('#cuerpo').load('pages_admin/default.php');"><a href="#mon_mod"><span class="glyphicon glyphicon-chevron-right"></span> Tipos de monedas</a></li>
-        <li onclick="$('#cuerpo').load('pages_admin/default.php');"><a href="#feriados"><span class="glyphicon glyphicon-chevron-right"></span> Feriados legales</a></li>                                                                                                             
+        <li onclick="$('#cuerpo').load('pages_admin/default.php');"><a href="#feriados"><span class="glyphicon glyphicon-chevron-right"></span> Feriados legales</a></li> 
+        <li class="nav-header">Información del Sistema</li>
+        <li onclick="$('#cuerpo').load('pages_admin/default.php');"><a href="#versión"><span class="glyphicon glyphicon-chevron-right"></span> Versión</a></li>  
+        <li onclick="$('#cuerpo').load('pages_admin/default.php');"><a href="#lic"><span class="glyphicon glyphicon-chevron-right"></span> Licencia</a></li>                                                                                                                    
       </ul>
     </div>
    </div>
