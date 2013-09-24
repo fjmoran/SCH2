d2ea6af66bb303f3609fdc3ac9f11975add3834f
fjmoran <fjmoran@gmail.com>
2013-09-24 12:48:05 -0300

    crear user

--- pages_admin/usr_crear.php
+++ pages_admin/usr_crear.php
@@ -2,6 +2,13 @@
  <h2>Crear Usuario</h2>
  <h5>Creación de nuevos usuarios en el sistema</h5><br>
 
-<?php require("usr_form_base.php") ?>
+<?php 
+$_GET[table] = "SCH2.Usuario";
+$_GET[select] = "nombreUsuario as Nombre, userUsuario as Usuario, claveUsuario as Clave, correoUsuario as Correo, telefonoUsuario as Telefono, celularUsuario as Movil, activoUsuario as Estado, Perfil_idPerfil as Rol";
+$_GET[jquery] = "$('#cuerpo').load('pages_admin/user_mod.php');";
+
+require("../recursos/zhi/insert_table_generic.php");
+
+?>
 
 </div><!-- col-md-11 -->
