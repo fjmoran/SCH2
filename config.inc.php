2bf279c51336de0b4e64cfcbb2a27eaca434bf6e
jaeirc <jaime.acevedo@gmail.com>
2013-09-24 18:10:00 -0300

    Archivo de configuración basicas
    Archivo que posee las configuraciones basicame del sistema

---
+++ config.inc.php
@@ -0,0 +1,9 @@
+<?php
+// Archivo de parametros minimos para el funcionamiento
+
+$INIT[basedir]="SCH2"; //directorio base donde se encuentra la aplicacion http://<host>/basedir/
+$INIT[schema]="SCH2"; //schema donde esta la base de datos del sistema
+$INIT[user]="root"; //usuario con que se conecta a la BD el usuario debe tener permisos de select en las tablas de sistema
+$INIT[password]="root"; //clave con que se conecta a la BD
+
+?>
