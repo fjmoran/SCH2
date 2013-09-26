12ae4d5a743c9a36d68c91e23ffbf368989cc39c
Francisco Morán <fjmoran@gmail.com>
2013-09-26 17:15:06 -0300

    con icono ahora pq el label no le gusto a JAE

--- pages_informes/default.php
+++ pages_informes/default.php
@@ -3,13 +3,13 @@
  	<h5>Esto es un texto de demo.</h5>
  	<div class="row">
 	 	<div class="col-md-6">
-	 		<h5>Titulo 1 <span class="label label-primary pull-right">Maximizar</span></h5>
+	 		<h5>Titulo 1<span class="glyphicon glyphicon-new-window pull-right"></span></h5>
 	 		<div class="graph-report">
 	 			<?php include "pages_informes/chart_01.php"; ?>	
 	 		</div>
 	 	</div>
 	 	<div class="col-md-6">
-	 		<h5>Titulo 2 <span class="label label-primary pull-right">Maximizar</span></h5>
+	 		<h5>Titulo 2<span class="glyphicon glyphicon-new-window pull-right"></span></h5>
 	 		<div class="graph-report">
 	 			<?php include "pages_informes/chart_02.php"; ?>	
 	 		</div>
@@ -17,13 +17,13 @@
 	 </div>
  	<div class="row">
 	 	<div class="col-md-6">
-	 		<h5>Titulo 3 <span class="label label-primary pull-right">Maximizar</span></h5>
+	 		<h5>Titulo 3<span class="glyphicon glyphicon-new-window pull-right"></span></h5>
 	 		<div class="graph-report">
 	 			<?php include "pages_informes/chart_03.php"; ?>		
 	 		</div>
 	 	</div>
 	 	<div class="col-md-6">
-	 		<h5>Titulo 4 <span class="label label-primary pull-right">Maximizar</span></h5>
+	 		<h5>Titulo 4<span class="glyphicon glyphicon-new-window pull-right"></span></h5>
 	 		<div class="graph-report">
 	 			<?php include "pages_informes/chart_04.php"; ?>			
 	 		</div>
