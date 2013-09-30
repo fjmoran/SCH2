916a93afa5c80494e38b14a0e21fc02d25aceff2
jaeirc <jaime.acevedo@gmail.com>
2013-09-30 11:44:14 -0300

    Con llaves primarias compuestas
    Se arregla para que funcione con llaves primarias compuestas

---
+++ recursos/zhi/insert_table_generic_v2.php
@@ -0,0 +1,168 @@
+<?php
+
+error_reporting(E_ALL);
+
+require_once "CreaConnv2.php";
+require "auth.php";
+
+$table = substr($_GET[table],strpos($_GET[table],".")+1);
+$schema = substr($_GET[table],0,strpos($_GET[table],"."));
+$select = "select ";
+
+if (isset($_GET[select])) { 
+	$select = $select . $_GET[select];
+}
+else { 
+	$select = $select . "*";
+}
+
+$select = $select ." from ". $_GET[table]; 
+
+if (isset($_GET[where])) { 
+	$select = $select." where ".$_GET[where];
+}
+
+if (isset($_GET[debug])) {echo $select ."</br>";}
+
+$rs = $mysqli->query($select);
+if (!$rs) {
+	echo "Falló al ejecutar la consulta: (". $mysqli->errno .") ". $mysqli->error;
+}
+
+if (isset($_GET[debug])) {
+	echo "Numero de Filas ". $rs->num_rows."</br>";
+	echo "====================</br>";
+}
+
+$info_campo = $rs->fetch_fields();
+if (isset($_GET[debug])){ print_r($info_campo);}
+$info_fila = $rs->fetch_assoc();
+if (isset($_GET[debug])){ print_r($info_fila);}
+$rs->free();
+
+$arreglo_campos_formulario = array();
+$llave_primaria = array();
+
+foreach ($info_campo as $valor) {
+  
+  $campo_formulario = "";
+
+  if (isset($_GET[debug])) {
+    printf("Nombre:        %s</br>", $valor->name);
+    printf("Tabla:         %s</br>", $valor->table);
+    printf("Longitud max.: %d</br>", $valor->max_length);
+    printf("Banderas:      %d</br>", $valor->flags);
+    printf("Tipo:          %d</br>", $valor->type);
+  }
+   switch ($valor->type) {
+    case 252:
+    case 253: $campo_formulario = "<label for=\"".$valor->orgname."\">".$valor->name.":</label><input id=\"".$valor->orgname."\" class=\"form-control\" type=\"text\" name=\"".$valor->orgname."\"";
+              if ((isset($_GET[where])) and ($rs->num_rows)){
+                $campo_formulario .= "value=\"".$info_fila[$valor->name]."\"";
+              }
+              $campo_formulario .= " >";
+              break;
+    case 1: $campo_formulario = "<label for=\"".$valor->orgname."\">".$valor->name.":</label><div class=\"checkbox\"><label><input type=\"checkbox\" value=\"1\" name=\"".$valor->orgname ."\"";
+            if ((isset($_GET[where])) and ($rs->num_rows)){
+              if ($info_fila[$valor->name]){
+                $campo_formulario .= "checked";
+              }
+            }else{
+              $campo_formulario .= " checked";
+            }
+            $campo_formulario .= " > Activo</label></div>";
+            break;
+    case 3:
+      if (!($valor->flags & 2)) {
+        // No es llave primaria, por lo que es una referencia.
+        if (isset($_GET[debug])) { echo "No es Llave Primaria</br>"; }
+        $query = "select REFERENCED_TABLE_NAME, REFERENCED_COLUMN_NAME from information_schema.key_column_usage where column_name = '".$valor->orgname."' and table_name='".$table."'";
+        if (isset($_GET[debug])) { echo $query."</br>"; }
+        $rsfk = $mysqli->query($query);
+        $info = $rsfk->fetch_assoc();
+        if (isset($_GET[debug])) { print_r ($info); echo "</br>"; }
+        $rsfk->free();
+
+      } else {
+        // Llave Primaria, puede ser compuesta o simple. En caso de ser compuesta hay que ver que se muestren los campos que son referencia de otra tabla.
+        // se crea arreglo con los datos de los campos de la llave despues se vera si se despliega o no.
+        if ($valor->flags & 16384) { 
+          if (isset($_GET[debug])) {echo "Es parte de una Llave ".$valor->orgname."</br>";}
+          array_push($llave_primaria,$valor->orgname);
+          if (isset($_GET[debug])) {print_r($llave_primaria); echo "</br>";}
+          $query = "select REFERENCED_TABLE_NAME, REFERENCED_COLUMN_NAME from information_schema.key_column_usage where column_name = '".$valor->orgname."' and table_name='".$table."'";
+          if (isset($_GET[debug])) { echo $query."</br>"; }
+          $rspk = $mysqli->query($query);
+          if ($rspk->num_rows > 1) {echo "Son mas de una fila </br>"; $rspk->data_seek(1);}
+          $info = $rspk->fetch_assoc();
+          if (isset($_GET[debug])) { print_r ($info); echo "</br>"; }
+          $rspk->free();
+
+        }
+      }
+
+      if($info[REFERENCED_TABLE_NAME] == NULL){
+        if (!($valor->flags & 512)){ 
+          $campo_formulario = "<label for=\"".$valor->orgname."\">".$valor->name.":</label><input id=\"".$valor->orgname."\" class=\"form-control\" type=\"text\" name=\"".$valor->orgname."\"";
+          if ((isset($_GET[where])) and ($rs->num_rows)){
+            $campo_formulario .= "value=\"".$info_fila[$valor->name]."\"";
+          }
+          $campo_formulario .= " >";
+        }
+      }else {
+        if (isset($_GET[debug])) { echo " Es un Foreign Key de la tabla ".$info[REFERENCED_TABLE_NAME]."</br>"; }
+        $select_list = "select ".$info[REFERENCED_COLUMN_NAME]." as id, nombre".$info[REFERENCED_TABLE_NAME]." as nombre from ".$schema.".".$info[REFERENCED_TABLE_NAME]." where activo".$info[REFERENCED_TABLE_NAME]."='1'";
+        if (isset($_GET[debug])) { echo "Query Foreign Key ".$select_list."</br>"; }
+        $rs_list_fk = $mysqli->query($select_list);
+        $campo_formulario = "<label for=\"".$valor->orgname."\">".$valor->name.":</label><select name=\"".$valor->orgname."\" class=\"form-control\">";
+        while ($list_fk=$rs_list_fk->fetch_assoc()) {
+          $campo_formulario .= "<option value=\"".$list_fk[id]."\">".$list_fk[nombre]."</option>";
+        }
+        $rs_list_fk->free();
+        $campo_formulario .= "</select>";
+      }
+
+      break;                   
+    }  
+  array_push($arreglo_campos_formulario, $campo_formulario);
+  if (isset($_GET[debug])) {echo "====================</br>";}
+}
+if (isset($_GET[debug])) {print_r ($arreglo_campos_formulario); echo "</br>";}
+
+?>
+	<form role="form" method="POST" action="recursos/zhi/insert_generic.php" target="IframeOutput">
+    <div class="row"> 
+      <div class="col-md-6"> <!-- columna uno -->
+        <?php
+          for ($i=0; $i <= count($arreglo_campos_formulario) -1; $i += 2){
+            echo "<div class=\"form-group\">";
+            echo $arreglo_campos_formulario[$i];
+            echo "</div>";
+          }
+        ?>
+      </div>
+    <div class="col-md-6"> <!-- columna dos -->
+        <?php
+          for ($i=1; $i <= count($arreglo_campos_formulario) -1; $i += 2){
+            echo "<div class=\"form-group\">";
+            echo $arreglo_campos_formulario[$i];
+            echo "</div>";
+          }
+        ?>
+    </div>
+  </div>
+
+  <div class="row pull-right"> <!-- fila para botones -->
+    <div class="col-md-12">
+      <p>
+        <button class="btn btn-default" type="reset" onclick= "<?php echo ($_GET[jquery]); ?>" >Cancelar</button>
+        <button class="btn btn-primary" type="submit">Guardar</button>
+      </p>
+    </div>
+  </div> 
+  <input type="hidden" name="table" value="<?php echo $_GET[table]; ?>">
+  <input type="hidden" name="select" value="<?php echo $_GET[select]; ?>">
+  <input type="hidden" name="jquery" value="<?php echo $_GET[jquery];?>">
+    <input type="hidden" name="debug" value="1">
+  
+</form>	
