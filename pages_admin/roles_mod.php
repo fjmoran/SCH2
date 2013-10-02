9b1b7097f7a7bad9865034e068a4aae13593fa0d
Francisco Morán <fjmoran@gmail.com>
2013-10-02 10:23:10 -0300

    se cambia a generador de tabla

--- pages_admin/roles_mod.php
+++ pages_admin/roles_mod.php
@@ -12,57 +12,15 @@ require "../recursos/zhi/auth.php";
 		<a onclick="$('#cuerpo').load('pages_admin/roles_crear.php');" href="#roles_crear" role="button" class="btn btn-sm btn-success pull-right"><span class="glyphicon glyphicon-plus-sign"></span> Agregar</a>
 
 	<h4>Roles del sistema</h4>
-	<table class="table table-striped table-bordered table-condensed">
-	  <thead>
-	    <tr>
-	      <th width=30%>Nombre del rol</th>
-	      <th width=45%>Descripción</th>
-	      <th width=15%>Estado</th>
-	      <th width=10%>Acciones</th>
-	    </tr>
-	  </thead>
-	  <tbody>
-	  	<?php
-		  	$query = "select * from SCH2.Perfil order by activoPerfil DESC, idPerfil";
-		  	if ($rs = $mysqli->query($query)){
-		  		if ($rs->num_rows > 0){
-		  			 // echo "En if num_rows</br>";
-		  			while($row = $rs->fetch_assoc()){
-		  				echo "<tr>\n";
-	  					echo "<td>".$row[nombrePerfil]."</td>\n";
-	  					echo "<td>".$row[descripcionPerfil]."</td>\n";
-		  				echo "<td><span class=\"label ";
-		  				if ($row[activoPerfil]){
-		  					echo "label-success";
-		  				}else {
-		  					echo "label-danger";
-		  				}
-		  				echo "\">";
-		  				if ($row[activoPerfil]){
-		  					echo "Activo";
-		  				}else {
-		  					echo "Inactivo";
-		  				}
-		  				echo "</span></td>\n";
-		  				echo "<td>\n";
-		  				echo "<a onclick=\"$('#cuerpo').load('pages_admin/roles_editar.php?idPerfil=".$row[idPerfil]."');\" href=\"#roles_editar\">";
-		  				echo "<span class=\"glyphicon glyphicon-pencil\" style=\"color: black;\" rel=\"tooltip\" data-toggle=\"tooltip\" title=\"Editar\"></span></a>";
-		  				if ($row[activoPerfil]){
-		  					echo "&nbsp;<span class=\"glyphicon glyphicon-remove-circle\" style=\"color: black;\" rel=\"tooltip\" data-toggle=\"tooltip\" title=\"Desactivar\"></span>";
-		  				}else {
-		  					echo "&nbsp;<span class=\"glyphicon glyphicon-refresh\" rel=\"tooltip\" data-toggle=\"tooltip\" title=\"Reactivar\"></span>";
-		  				}
-		  				echo "</td>\n";	  					
-		  				echo "</tr>\n";
-		  			}
-		  		}
-		  	}else{
-		  		echo "Falló al ejecutar la consulta: (". $mysqli->errno .") ". $mysqli->error;
-		  	}
-	  	?>
 
-	  </tbody>
-	</table>
+	<?php 
+	$_GET[table] = $bd.".Perfil";
+	$_GET[select] = "nombrePerfil as Nombre, descripcionPerfil as Descripcion, activoPerfil as Estado";
+	$_GET[tabla][width] = "30%, 45%, 15%";
+	$_GET[tabla][title] = "Nombre del rol, Descripción, Estado";
+	require("../recursos/zhi/table_generator.php");
+
+	?>
 
 	<div class="col-md-12 text-center">
 	  <ul class="pagination pagination-sm" >
