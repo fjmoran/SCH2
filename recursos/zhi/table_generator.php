e4dd1640ee69ef9c5a575fb46a04ecc58b90a0c2
jaeirc <jaime.acevedo@gmail.com>
2013-10-02 17:53:03 -0300

    Llaves Foraneas
    Se arregla para que las llaves foraneas muestre el valor.
    Unica restricción el campo de valor de llave foraneas debe llamarse
    nombre<Tabla>

--- recursos/zhi/table_generator.php
+++ recursos/zhi/table_generator.php
@@ -11,8 +11,9 @@ $schema = substr($_GET['table'],0,strpos($_GET['table'],"."));
 $select = "select ";
 
 $body_table = ""; //se guardan los campos que van en body de la tabla
-$header_table = array(); //se guardan los campos que van en header de la tabla
+$header_table = ""; //se guardan los campos que van en header de la tabla
 
+//Obtención de llaves primarias
 $show_key = "show keys from ".$_GET['table']." where key_name = 'PRIMARY'";
 
 if (isset($_GET['debug'])) {echo "Query para buscar llaves primarias ".$show_key."</br>";}
@@ -22,13 +23,32 @@ if ($rs_show = $mysqli->query($show_key)){
 	while ($row = $rs_show->fetch_assoc()){
 		array_push($keys,$row['Column_name']);
 	}
-	$rs_show->free();
 	if (isset($_GET['debug'])) {echo "Resultado de la query que retorna el listado de llaves primarias ";print_r($keys); echo "</br>";}
 
 }else {
 	echo "Falló al ejecutar la consulta: (". $mysqli->errno .") ". $mysqli->error;
 }
+$rs_show->free();
 
+// Obtención de Llaves Foraneas
+$query_fk = "SELECT k.REFERENCED_TABLE_NAME, k.REFERENCED_COLUMN_NAME, k.COLUMN_NAME\n"
+    . "FROM information_schema.TABLE_CONSTRAINTS i \n"
+    . "LEFT JOIN information_schema.KEY_COLUMN_USAGE k ON i.CONSTRAINT_NAME = k.CONSTRAINT_NAME \n"
+    . "WHERE i.CONSTRAINT_TYPE ='FOREIGN KEY'AND i.TABLE_SCHEMA ='".$schema."' AND i.TABLE_NAME ='".$table."' order by k.REFERENCED_TABLE_NAME";
+
+if (isset($_GET['debug'])) {echo "Query para buscar llaves foraneas ".$query_fk."</br>";}
+
+if ($rs_fk = $mysqli->query($query_fk)){
+	$fkeys = array();
+	while ($row = $rs_fk->fetch_assoc()){
+		$fkeys[$row['COLUMN_NAME']] = array($row['REFERENCED_TABLE_NAME'],$row['REFERENCED_COLUMN_NAME']);
+	}
+	if (isset($_GET['debug'])) {echo "Resultado de la query que retorna el listado de llaves foraneas ";print_r($fkeys); echo "</br>";}
+
+}else {
+	echo "Falló al ejecutar la consulta: (". $mysqli->errno .") ". $mysqli->error;
+}
+$rs_fk->free();
 
 // Generacion del select como el usuario lo solicita
 
@@ -47,6 +67,10 @@ if (isset($_GET['where'])) {
 
 if (isset($_GET['orderby'])) { 
 	$select = $select." order by ".$_GET['orderby'];
+}
+
+if (isset($_GET['limit'])) {
+	$select = $select." LIMIT ".$_GET['limit'];
 }
 
 if (isset($_GET['debug'])) {echo "query que viene del usuario ".$select ."</br>";}
@@ -89,7 +113,7 @@ if (isset($_GET['tabla']['width'])){
 	if (isset($_GET['debug'])) {echo "Ancho de columnas que entrego el usuario ";print_r ($title_width); echo "</br>";}
 }
 
-
+// Chequeo del las PRIMARY KEY en el select enviado por el usuario, en caso que no esten las PRIMARY KEY se agregan a la query y se ejecuta nuevamente.
 
 $regen_select = false; // variable para indicar si hay que regenerar la query
 
@@ -98,12 +122,44 @@ foreach ($keys as $key){
 		if (isset($_GET['debug'])) {echo "Se encontro la llave ".$key." </br>";}
 	}else{
 		if (isset($_GET['debug'])) {echo "No se econtro la llave ".$key." </br>";}
+		if (!(isset($_GET['select']))) { $_GET['select'] = $table.".*";}
 		$_GET['select'] .= ",".$key." ";
 		$regen_select = true;
 	}
 }
 
-if ($regen_select) {
+//Cheque llaves Foraneas en el select del usuario
+$left_join	= array();
+$last_table = "";
+foreach ($fkeys as $fkey=>$value){
+	if (in_array($fkey,$column_select)){
+		
+		if (isset($_GET['debug'])) {echo "Se encontro la llave foranea ".$fkey." </br>";}
+		
+		if ($last_table == $value[0]){
+			
+			$left_join[sizeof($left_join)-1] .= " AND ".$value[0].".".$value[1]."=".$table.".".$fkey;
+			if (isset($_GET['debug'])) {
+				echo "Misma Tabla ".$value[0]." ";
+				print_r ($left_join);
+				echo "</br>";
+			}
+		}else{
+				array_push ($left_join,"LEFT JOIN ".$value[0]." ON ".$value[0].".".$value[1]."=".$table.".".$fkey);
+			}
+		
+		if (!(isset($_GET['select']))) { $_GET['select'] = $table.".*";}
+		
+		$_GET['select'] .= ", ".$value[0].".nombre".$value[0]." ";
+		$pos = array_search($fkey,$title_select);
+		$title_select[$pos] = "nombre".$value[0];
+		$last_table = $value[0];
+		$regen_select = true;
+	}
+}
+if (isset($_GET['debug'])) {echo "Arreglo de Left join: "; print_r($left_join); echo "</br>";}
+
+if ($regen_select) { // Si falto algun PRIMARY KEY o hay alguna llave foranea, se agrego a la consulta y se ejecuta nuevamente.
 	$select = "select ";
 	if (isset($_GET['select'])) { 
 		$select = $select . $_GET['select'];
@@ -113,6 +169,12 @@ if ($regen_select) {
 	}
 
 	$select = $select ." from ". $_GET['table']; 
+
+	if (!(empty($left_join))){
+		$select = $select ." ". implode(" ",$left_join);
+	}
+
+	echo $select."</br>";
 
 	if (isset($_GET['where'])) { 
 		$select = $select." where ".$_GET['where'];
@@ -126,17 +188,39 @@ if ($regen_select) {
 	// Se libera el result de la query con select del usuario
 	$rs->free();
 
-	// se ejecuta el query con los id faltantes
+	// se ejecuta el query con las PRIMARY KEY faltantes
 	$rs = $mysqli->query($select);
+	if (!$rs) {
+		echo "Falló al ejecutar la consulta: (". $mysqli->errno .") ". $mysqli->error;
+	}
 }
 
 if ($rs->num_rows > 0){
 	 if (isset($_GET['debug'])) {echo "En if num_rows</br>";}
 }
 
+//Llenado de $header_table
+$header_table = "<tr>\n";
+for($i = 0; $i < count($title_table); $i++){
+	$header_table.= "<th ";
+	if (isset($_GET['tabla']['width'])){
+		$_title_table .= "width='".$title_width[$i]."'";
+	}
+	$header_table .= " >";
+	$header_table .= htmlentities($title_table[$i],ENT_SUBSTITUTE,'UTF-8');
+	$header_table .= "</th>\n";
+}
+if (isset($_GET['acciones'])){
+	$header_table .= "<th width=10%>Acciones</th>\n";
+}
+$header_table .= "</tr>\n";
+
+//Llenado de los datos de la tabla.
 $campos_tabla = array();
-while ($row = $rs->fetch_assoc()) {array_push($campos_tabla,$row);}
-$campos_tabla_info = $rs->fetch_fields();
+
+while ($row = $rs->fetch_assoc()) {array_push($campos_tabla,$row);} // se saca toda la información resultante de la query en la variable $campos_tabla
+
+$campos_tabla_info = $rs->fetch_fields(); // Se obtiene la descripcion de los campos que tiene la query
 
 $rs->free();
 
@@ -144,7 +228,7 @@ if (isset($_GET['debug'])) {echo "Listado de datos para la tabla : "; print_r($c
 if (isset($_GET['debug'])) {echo "Listado de campos que trae la tabla : ";print_r($campos_tabla_info); echo "</br>";}
 
 $campo_tipo = array(); // arreglo con los tipos de las columnas en la tabla
-foreach ($campos_tabla_info as $valor){
+foreach ($campos_tabla_info as $valor){ // se extraen los tipos d
 	$campo_tipo[$valor->name]=$valor->type;
 }
 
