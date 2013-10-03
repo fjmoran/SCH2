4a809a5f3fa0b3f6b3af4c070dcd59e078842c3b
Francisco Morán <fjmoran@gmail.com>
2013-10-03 16:44:07 -0300

    se saca tabla en duro

--- pages_admin/mat_mod.php
+++ pages_admin/mat_mod.php
@@ -22,50 +22,6 @@
 	require("../recursos/zhi/table_generator.php");
 	?>
 
-	<table class="table table-striped table-bordered table-condensed">
-	  <thead>
-	    <tr>
-	      <th width=25%>Nombre</th>
-	      <th width=25%>Cliente</th>
-	      <th width=25%>Tipo</th>
-	      <th wigth=15%>Estado</th>
-	      <th width=10%>Acciones</th>
-	    </tr>
-	  </thead>
-	  <tbody>
-	    <tr>
-	      <td>Materia 1</td>    	
-	      <td>Codelco</td>
-	      <td>Tipo materia 1</td>
-	      <td><span class="label label-danger">Inactivo</span></td>
-	      <td><span class="glyphicon glyphicon-refresh" rel="tooltip" data-toggle="tooltip" title="Reactivar"></span></td>
-	    </tr>
-	    <tr>
-	      <td>Materia 2</td>      	
-	      <td>Cencosud</td>
-	      <td>Tipo materia 5</td>
-	      <td><span class="label label-danger">Inactivo</span></td>      
-	      <td><span class="glyphicon glyphicon-refresh" rel="tooltip" data-toggle="tooltip" title="Reactivar"></span></td>
-	    </tr>
-	    <tr>
-	      <td>Materia 3</td>    	
-	      <td>Parque Arauco</td>
-	      <td>Tipo materia 1</td>
-	      <td><span class="label label-success">Activo</span></td>
-	      <td><a onclick="$('#cuerpo').load('pages_admin/mat_editar.php');" href="#usr_editar"><span class="glyphicon glyphicon-pencil" style="color: black;" rel="tooltip" data-toggle="tooltip" title="Editar"></span></a>	
-	      <span class="glyphicon glyphicon-remove-circle" style="color: black;" rel="tooltip" data-toggle="tooltip" title="Desactivar"></span></td>
-	    </tr>
-	    <tr>
-	      <td>Materia 4</td>     	
-	      <td>Andres de la Fontana</td>
-	      <td>Tipo materia 56</td>
-	      <td><span class="label label-success">Activo</span></td>
-	      <td><a onclick="$('#cuerpo').load('pages_admin/mat_editar.php');" href="#usr_editar"><span class="glyphicon glyphicon-pencil" style="color: black;" rel="tooltip" data-toggle="tooltip" title="Editar"></span></a>	      	
-	      <span class="glyphicon glyphicon-remove-circle" style="color: black;" rel="tooltip" data-toggle="tooltip" title="Desactivar"></span></td>
-	    </tr>   
-	  </tbody>
-	</table>
-
 	<div class="col-md-12 text-center">
 	  <ul class="pagination pagination-sm" >
 	    <li><a href="#">Anterior</a></li>
@@ -78,13 +34,7 @@
 	  </ul>
 	</div>
 
-
-
-
-
 </div><!-- col-md-11 -->
-
-
 
     <script type="text/javascript"> 
       
