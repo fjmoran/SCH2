a920a0d9460e7b330a9c8b12d56de04a86470946
Francisco Morán <fjmoran@gmail.com>
2013-10-08 11:20:17 -0300

    feriados_mod

--- pages_admin/feriados_mod.php
+++ pages_admin/feriados_mod.php
@@ -6,42 +6,21 @@
 		<a onclick="$('#cuerpo').load('pages_admin/feriados_crear.php');" href="#feriados_crear" role="button" class="btn btn-sm btn-success pull-right"><span class="glyphicon glyphicon-plus-sign"></span> Agregar</a>
 
 	<h4>Días feriados registrados</h4>
-	<table class="table table-striped table-bordered table-condensed">
-	  <thead>
-	    <tr>
-	      <th width=25%>Fecha</th>
-	      <th width=25%>Descripción</th>
-	      <th width=25%>Tipo</th>
-	      <th wigth=15%>Estado</th>
-	      <th width=10%>Acciones</th>
-	    </tr>
-	  </thead>
-	  <tbody>
-	    <tr>
-	      <td>01-01-2014</td>    	
-	      <td>Año Nuevo</td>
-	      <td>Feriado legal</td>
-	      <td><span class="label label-danger">Inactivo</span></td>
-	      <td><span class="glyphicon glyphicon-refresh" rel="tooltip" data-toggle="tooltip" title="Reactivar"></span></td>
-	    </tr>
-	    <tr>
-	      <td>25-12-2013</td>    	
-	      <td>Navidad</td>
-	      <td>JO JO JO</td>
-	      <td><span class="label label-success">Activo</span></td>
-	      <td><a onclick="$('#cuerpo').load('pages_admin/feriado_editar.php');" href="#feriado_editar"><span class="glyphicon glyphicon-pencil" style="color: black;" rel="tooltip" data-toggle="tooltip" title="Editar"></span></a>	
-	      <span class="glyphicon glyphicon-remove-circle" style="color: black;" rel="tooltip" data-toggle="tooltip" title="Desactivar"></span></td>
-	    </tr>
-	    <tr>
-	      <td>18-09-2013</td>     	
-	      <td>el 18!</td>
-	      <td>Tiki Tiki Ti</td>
-	      <td><span class="label label-success">Activo</span></td>
-	      <td><a onclick="$('#cuerpo').load('pages_admin/feriado_editar.php');" href="#feriado_editar"><span class="glyphicon glyphicon-pencil" style="color: black;" rel="tooltip" data-toggle="tooltip" title="Editar"></span></a>	      	
-	      <span class="glyphicon glyphicon-remove-circle" style="color: black;" rel="tooltip" data-toggle="tooltip" title="Desactivar"></span></td>
-	    </tr>   
-	  </tbody>
-	</table>
+
+	<?php 
+	$_GET['table'] = $bd.".Feriado";
+	$_GET['select'] = "fechaFeriado as Fecha, descripcionFeriado as Descripcion, tipoFeriado as Tipo, Pais_idPais as Pais, activoFeriado as Estado";
+	$_GET['orderby'] = "activoFeriado DESC, Pais_idPais";
+	$_GET['tabla']['width'] = "15%, 30%, 15%, 15%, 15%";
+	$_GET['tabla']['title'] = "Fecha, Descripción, Tipo, País, Estado";
+	$_GET['acciones'] = "true";
+	$_GET['accion']['editar']['URL'] = "pages_admin/feriados_editar.php";
+	$_GET['accion']['editar']['title'] = "Editar";
+	$_GET['accion']['editar']['class'] = "glyphicon glyphicon-pencil";
+	$_GET['accion']['activar']['URL'] = "pages_admin/roles_estado.php";
+
+	require("../recursos/zhi/table_generator.php");
+	?>
 
 	<div class="col-md-12 text-center">
 	  <ul class="pagination pagination-sm" >
@@ -54,10 +33,6 @@
 	    <li><a href="#">Siguiente</a></li>
 	  </ul>
 	</div>
-
-
-
-
 
 </div><!-- col-md-11 -->
 
