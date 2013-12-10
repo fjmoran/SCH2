60110c43d93191b8c3000fb37bdc98596f71218c
jaeirc <jaime.acevedo@gmail.com>
2013-12-10 09:14:51 -0300

    desactivar usuario logeado
    Se elimina la opción de desactivar el usuario logeado en la pantalla de
    editar

--- recursos/zhi/insert_table_generic_v2.php
+++ recursos/zhi/insert_table_generic_v2.php
@@ -75,16 +75,19 @@ foreach ($info_campo as $valor) {
               if ($valor->flags & 1) { array_push($validacion,$valor->orgname); }
               break;
               
-    case 1: $campo_formulario = "<label for=\"".$valor->orgname."\">".htmlentities($valor->name,ENT_SUBSTITUTE,'UTF-8').":</label><div class=\"checkbox\"><label><input type=\"checkbox\" value=\"1\" name=\"".$valor->orgname ."\"";
-            if ((isset($_GET['where'])) and ($_GET['edit'])){
-              if ($info_fila[$valor->name]){
-                $campo_formulario .= "checked";
+    case 1: 
+            if ((isset($_GET['idUsuario'])) && ($_GET['idUsuario'] != $_SESSION['idUsuario'])) {
+              $campo_formulario = "<label for=\"".$valor->orgname."\">".htmlentities($valor->name,ENT_SUBSTITUTE,'UTF-8').":</label><div class=\"checkbox\"><label><input type=\"checkbox\" value=\"1\" name=\"".$valor->orgname ."\"";
+              if ((isset($_GET['where'])) and ($_GET['edit'])){
+                if ($info_fila[$valor->name]){
+                  $campo_formulario .= "checked";
+                }
+              }else{
+                $campo_formulario .= " checked";
               }
-            }else{
-              $campo_formulario .= " checked";
+              $campo_formulario .= " > Activo</label></div>";
+              #if ($valor->flags & 1) { array_push($validacion, $valor->orgname);}
             }
-            $campo_formulario .= " > Activo</label></div>";
-            #if ($valor->flags & 1) { array_push($validacion, $valor->orgname);}
             break;
 
     case 10:
