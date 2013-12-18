55bb5ae5ac471394a4bf92a78ee8180970ec4ee3
jaeirc <jaime.acevedo@gmail.com>
2013-12-18 12:43:20 -0300

    arreglado problema de telefonos
    Se arregla problema que actualizar telefonos

--- recursos/zhi/actualizar_usr.php
+++ recursos/zhi/actualizar_usr.php
@@ -2,7 +2,7 @@
 require_once("auth.php");
 require_once("CreaConnv2.php");
 
-$update_Usuario = "update Usuario set nombreUsuario = '".$mysqli->real_escape_string($_POST['nombreUsuario'])."', correoUsuario = '".$mysqli->real_escape_string($_POST['correoUsuario'])."', telefonoUsuario = '".$mysqli->real_escape_string($_POST['telefono'])."', celularUsuario = '".$mysqli->real_escape_string($_POST['celular'])."' where idUsuario = '".$_SESSION['idUsuario']."'";
+$update_Usuario = "update Usuario set nombreUsuario = '".$mysqli->real_escape_string($_POST['nombreUsuario'])."', correoUsuario = '".$mysqli->real_escape_string($_POST['correoUsuario'])."', telefonoUsuario = '".$mysqli->real_escape_string($_POST['telefonoUsuario'])."', celularUsuario = '".$mysqli->real_escape_string($_POST['celularUsuario'])."' where idUsuario = '".$_SESSION['idUsuario']."'";
 
 if($mysqli->query($update_Usuario)){
 	printf("%d fila insertada.\n", $mysqli->affected_rows);
