9f57670b259aad8a427748bdafdb68d932a13eec
jaeirc <jaime.acevedo@gmail.com>
2014-01-09 15:43:47 -0300

    Se solución agrega campo
    Se agrega campo al insert ya que el modelo cambio y se le agregó el
    campo idPaginaenPagina en la tabla pero no se había agregado al insert

--- recursos/zhi/update_paginas_paginas.php
+++ recursos/zhi/update_paginas_paginas.php
@@ -2,10 +2,11 @@
 require_once("auth.php");
 require_once("CreaConnv2.php");
 
-//echo "_POST : ";
-//print_r($_POST);
-//echo "</br>";
-
+/*
+echo "_POST : ";
+print_r($_POST);
+echo "</br>";
+*/
 
 
 $query = "";
@@ -27,14 +28,16 @@ if($result = $mysqli->query($paginas_existentes)){
 
 $result->free();
 
-//echo "paginas :";
-//print_r($paginas);
-//echo "</br> Pagina Padre : $padre - Exito</br>";
+/*
+echo "paginas :";
+print_r($paginas);
+echo "</br> Pagina Padre : $padre - Exito</br>";
+*/
 
 //Busco si las paginas que vienen en la web ya estaban ingresada, en caso que no agrego el INSERT correspondiente
 foreach ($paginas as $idPagina1){
 	if (!(in_array($idPagina1, $paginas_en_paginas))){
-		$query .= "INSERT INTO PaginaenPagina VALUES ('".$padre."','".$idPagina1."');";
+		$query .= "INSERT INTO PaginaenPagina VALUES ('','".$padre."','".$idPagina1."');";
 	}
 }
 
@@ -61,7 +64,6 @@ if ($mysqli->multi_query($query)) {
         }
     } while ($mysqli->next_result());
 }
-
 ?>
 
 <h2>Se ha terminado con exito la actualización de la tabla PaginaenPagina.</h2>
