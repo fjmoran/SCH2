4bb44bb49900f568f6ae6c91b0d7d3fb67f424b0
Francisco Morán <fjmoran@gmail.com>
2014-01-09 16:17:23 -0300

    alert

--- pages_admin/paginas_paginas_mod.php
+++ pages_admin/paginas_paginas_mod.php
@@ -3,65 +3,77 @@ require_once("../recursos/zhi/auth.php");
 require_once("../recursos/zhi/CreaConnv2.php");
 require_once("../recursos/zhi/funciones.php");
 ?>
-					<div class="col-md-11">
-					 	<h2>Relaciones entre Páginas</h2>
-					 	<h5>Seleccione la pagina que desea configurar y arrastre las paginas de la columna izquierda a la derecha para indicar que dichas paginas estan relacionadas.</h5><br>
-					 	<form role="form"  name="selpagina" id="selpagina" action="pages_admin/paginas_paginas_mod.php" method="POST">
-						 	<div class="row">
-						 		<div class="col-md-5">
-								 	<div class="form-group">
-							          <label for="rol">Página:</label>
-							            <select id="pagina" class="form-control" name="pagina">
-							            	<option value="">&nbsp</option>
-							            	<?php
-							            		echo option_select("Pagina","nombrePagina","idPagina",$mysqli,$_POST['pagina']);
-							            	?>
-							            </select>          
-							        </div>
-						 		</div>
-						 		<div class="col-md-7"> <!-- columna vacia -->
-						 		</div>
-							</div>
-						</form>
-						 	<? 
-								if ((isset($_POST['pagina'])) && (!empty($_POST['pagina'])))
-								{
-							?>
-						<form role="form"  name="paginasenpagina" id="paginasenpagina" method="POST">	
-						 	<div class="row">		
-						 		<div class="col-md-5">
-						 			<h5>Páginas disponibles</h5>
-						 			<ul id="sortable1" class="droptrue">
-						 				<?php
-						 					echo listado("ui-state-default","Pagina","nombrePagina","idPagina",$mysqli,"Pagina.idPagina NOT IN (select Pagina_idPagina1 from PaginaenPagina where Pagina_idPagina ='".$_POST['pagina']."')");
-						 				?>
-									</ul>
-								</div>
-						 		<div class="col-md-5">
-						 			<h5>Páginas relacionadas</h5>		
-									<ul id="sortable2" class="droptrue">
-										<?php
-											echo listado("ui-state-default","Pagina","nombrePagina","idPagina",$mysqli,"Pagina.idPagina IN (select Pagina_idPagina1 from PaginaenPagina where Pagina_idPagina ='".$_POST['pagina']."')");
-										?>
-									</ul>
-						 		</div>
-						 		<div class="col-md-2"> <!-- columna vacia -->
-						 		</div>		
-						 	</div>
-							<div class="row">
-							    <div class="row pull-right"> <!-- fila para botones -->
-							      <div class="col-md-12">
-							        <p>
-							          <input class="btn btn-primary" type="submit" value="Guardar" id="frmboton">
-							        </p>
-							      </div>
-							    </div>  
-							<div>     
-					 	</form>
-					 	<?php
-					 	}
-					 	?>
-					</div><!-- col-md-11 -->
+
+<div class="col-md-11">
+
+ 	<h2>Relaciones entre Páginas</h2>
+ 	<h5>Seleccione la pagina que desea configurar y arrastre las paginas de la columna izquierda a la derecha para indicar que dichas paginas estan relacionadas.</h5><br>
+ 	<form role="form"  name="selpagina" id="selpagina" action="pages_admin/paginas_paginas_mod.php" method="POST">
+
+	<div class="row">
+		<div class="col.md-12">
+			<div id="alert-insert" class="alert alert-success alert-dismissable hide">
+		  		<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+		  		Se ha realizado con exito la actualización de la tabla.
+			</div>
+		<div>	
+	</div>
+
+	 	<div class="row">
+	 		<div class="col-md-5">
+			 	<div class="form-group">
+		          <label for="rol">Página:</label>
+		            <select id="pagina" class="form-control" name="pagina">
+		            	<option value="">&nbsp</option>
+		            	<?php
+		            		echo option_select("Pagina","nombrePagina","idPagina",$mysqli,$_POST['pagina']);
+		            	?>
+		            </select>          
+		        </div>
+	 		</div>
+	 		<div class="col-md-7"> <!-- columna vacia -->
+	 		</div>
+		</div>
+	</form>
+	 	<? 
+			if ((isset($_POST['pagina'])) && (!empty($_POST['pagina'])))
+			{
+		?>
+	<form role="form"  name="paginasenpagina" id="paginasenpagina" method="POST">	
+	 	<div class="row">		
+	 		<div class="col-md-5">
+	 			<h5>Páginas disponibles</h5>
+	 			<ul id="sortable1" class="droptrue">
+	 				<?php
+	 					echo listado("ui-state-default","Pagina","nombrePagina","idPagina",$mysqli,"Pagina.idPagina NOT IN (select Pagina_idPagina1 from PaginaenPagina where Pagina_idPagina ='".$_POST['pagina']."')");
+	 				?>
+				</ul>
+			</div>
+	 		<div class="col-md-5">
+	 			<h5>Páginas relacionadas</h5>		
+				<ul id="sortable2" class="droptrue">
+					<?php
+						echo listado("ui-state-default","Pagina","nombrePagina","idPagina",$mysqli,"Pagina.idPagina IN (select Pagina_idPagina1 from PaginaenPagina where Pagina_idPagina ='".$_POST['pagina']."')");
+					?>
+				</ul>
+	 		</div>
+	 		<div class="col-md-2"> <!-- columna vacia -->
+	 		</div>		
+	 	</div>
+		<div class="row">
+		    <div class="row pull-right"> <!-- fila para botones -->
+		      <div class="col-md-12">
+		        <p>
+		          <input class="btn btn-primary" type="submit" value="Guardar" id="frmboton">
+		        </p>
+		      </div>
+		    </div>  
+		<div>     
+ 	</form>
+ 	<?php
+ 	}
+ 	?>
+</div><!-- col-md-11 -->
 
 <script>
 $(function(){
