e83c57e930a0272b7243d5b4851c6d6d23d11ccb
Francisco Morán M <fjmoran@gmail.com>
2014-01-10 13:09:31 -0300

    con los insert de menu y pagina

--- sql_scripts/SCH2.sql
+++ sql_scripts/SCH2.sql
@@ -1392,6 +1392,7 @@ INSERT INTO `SCH2`.`Pagina` (`idPagina`, `urlPagina`, `nombrePagina`, `descripci
 INSERT INTO `SCH2`.`Pagina` (`idPagina`, `urlPagina`, `nombrePagina`, `descripcionPagina`, `activoPagina`) VALUES (63, 'usr_editar.php', 'pages_admin/usr_editar.php', 'EdiciÃ³n de Usuario', 1);
 INSERT INTO `SCH2`.`Pagina` (`idPagina`, `urlPagina`, `nombrePagina`, `descripcionPagina`, `activoPagina`) VALUES (64, 'usr_mi_perfil.php', 'pages_admin/usr_mi_perfil.php', 'EdiciÃ³n de Perfil del usuario', 1);
 INSERT INTO `SCH2`.`Pagina` (`idPagina`, `urlPagina`, `nombrePagina`, `descripcionPagina`, `activoPagina`) VALUES (65, 'usr_tar_mod.php', 'pages_admin/usr_tar_mod.php', 'EdiciÃ³n de Tarifa del usuario', 1);
+INSERT INTO `SCH2`.`Pagina` (`idPagina`, `urlPagina`, `nombrePagina`, `descripcionPagina`, `activoPagina`) VALUES (66,'#paginas_paginas_mod','pages_admin/paginas_paginas_mod.php','Relaciones entre pÃ¡ginas del sistema',1);
 
 COMMIT;
 
@@ -1405,6 +1406,31 @@ INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `p
 INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (2, 'Informes', 0, 1, 2, 'glyphicon glyphicon-file', 2, NULL);
 INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (3, 'AdministraciÃ³n', 0, 1, 3, 'glyphicon glyphicon-wrench', 3, NULL);
 INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (4, 'Salir', 0, 1, 4, 'glyphicon glyphicon-off', 4, NULL);
+INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (5,'Mantenedores',1,1,1,'',NULL,3);
+INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (6,'Usuarios',1,1,2,'glyphicon glyphicon-chevron-right',5,3);
+INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (7,'Roles',1,1,3,'glyphicon glyphicon-chevron-right',6,3);
+INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (8,'Permisos por rol',1,1,4,'glyphicon glyphicon-chevron-right',7,3);
+INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (9,'Tipos de contacto',1,1,5,'glyphicon glyphicon-chevron-right',8,3);
+INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (10,'Materias',1,1,6,'glyphicon glyphicon-chevron-right',9,3);
+INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (11,'Tipos de materia',1,1,7,'glyphicon glyphicon-chevron-right',10,3);
+INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (12,'Tarifas por materia',1,1,8,'glyphicon glyphicon-chevron-right',11,3);
+INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (13,'Estados de factura',1,1,9,'glyphicon glyphicon-chevron-right',12,3);
+INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (14,'Tipos de moneda',1,1,10,'glyphicon glyphicon-chevron-right',13,3);
+INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (15,'Tipos de abono',1,1,11,'glyphicon glyphicon-chevron-right',14,3);
+INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (16,'Tipo de gasto',1,1,12,'glyphicon glyphicon-chevron-right',15,3);
+INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (17,'Feriados legales',1,1,13,'glyphicon glyphicon-chevron-right',16,3);
+INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (18,'ParÃ¡metros',1,1,14,'',NULL,3);
+INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (19,'PaÃ­ses',1,1,15,'glyphicon glyphicon-chevron-right',17,3);
+INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (20,'Regiones',1,1,16,'glyphicon glyphicon-chevron-right',18,3);
+INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (21,'Comunas',1,1,17,'glyphicon glyphicon-chevron-right',19,3);
+INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (22,'Labels',1,1,18,'glyphicon glyphicon-chevron-right',20,3);
+INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (23,'MenÃº',1,1,19,'glyphicon glyphicon-chevron-right',21,3);
+INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (24,'PÃ¡ginas',1,1,20,'glyphicon glyphicon-chevron-right',22,3);
+INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (25,'ParÃ¡metros Globales',1,1,21,'glyphicon glyphicon-chevron-right',23,3);
+INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (26,'InformaciÃ³n del Sistema',1,1,23,'glyphicon glyphicon-chevron-right',NULL,3);
+INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (27,'VersiÃ³n',1,1,24,'glyphicon glyphicon-chevron-right',24,3);
+INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (28,'Licencia',1,1,25,'glyphicon glyphicon-chevron-right',25,3);
+INSERT INTO `SCH2`.`Menu` (`idMenu`, `nombreMenu`, `nivelMenu`, `activoMenu`, `posicionMenu`, `spanclassMenu`, `Pagina_idPagina`, `Menu_idMenu`) VALUES (29,'Mapa de pÃ¡ginas',1,1,22,'glyphicon glyphicon-chevron-right',66,3);
 
 COMMIT;
 
